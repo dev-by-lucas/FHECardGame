33431c382a5c3f1381b18d01ba28c992f04719fe
dev-by-lucas <rypzhuw93572@outlook.com>
2025-10-28 14:05:26 +0800

    deploy contract

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedCardGame = await deploy("CardGame", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`CardGame contract: `, deployedCardGame.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_card_game"; // id required to prevent reexecution
+func.tags = ["CardGame"];
